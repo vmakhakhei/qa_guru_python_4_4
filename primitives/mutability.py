5b2e16133133e0826781123f24be7554f5b0b185
skhomuti <skhomuti@gmail.com>
2023-02-27 21:18:48 +0500

    Инициализация репозитория

---
+++ primitives/mutability.py
@@ -0,0 +1,27 @@
+# Списки, словари и множества - изменяемые!
+
+from copy import deepcopy
+
+
+a = [1, 2, 3]
+b = a.copy()
+b.append(4)
+
+print(a)
+print(b)
+
+
+a = [1, 2, 3, [4, 5, 6]]
+b = deepcopy(a)
+b[-1].append(7)
+
+print(a)
+print(b)
+
+
+# Кортежи, frozenset  - нет
+
+a = (1, 2, 3, "4", "5")
+d = {"key": "value", "first": "second"}
+dd = (("key", "value"), ("first", "second"))
+print(d)
