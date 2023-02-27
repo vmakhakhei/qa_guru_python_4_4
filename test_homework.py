7693ae39422b3487873fbd3681a6fc0dfd7d33c8
VMakhakhei <silenthealsqwe@gmail.com>
2023-02-27 23:40:49 +0100

    first

--- test_homework.py
+++ test_homework.py
@@ -1,10 +1,12 @@
+import math
+import random
 
 
 def test_greeting():
     name = "Анна"
     age = 25
     # TODO Сформируйте нужную строку
-    output = ""
+    output = f"Привет, {name}! Тебе {age} лет."
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
 
@@ -18,11 +20,11 @@ def test_rectangle():
     b = 20
 
     # TODO сосчитайте периметр
-    perimeter = 0
+    perimeter = (a+b)*2
     assert perimeter == 60
 
     # TODO сосчитайте площадь
-    area = 0
+    area = a*b
     assert area == 200
 
 
@@ -32,11 +34,11 @@ def test_circle():
     """
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = math.pi*r**2
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = (2*math.pi)*r
     assert length == 144.29090290269986
 
 
@@ -46,7 +48,8 @@ def test_random_list():
     """
 
     # TODO создайте список
-    l = []
+    l = [random.randint(1,100) for i in range(10)]
+    l = sorted(l)
     assert len(l) == 10
     assert l[0] < l[-1]
 
@@ -57,7 +60,8 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
-
+    l = set(l)
+    l = list(l)
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -73,7 +77,7 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = {}
+    d = dict(zip(first,second))
 
     assert isinstance(d, dict)
     assert len(d) == 5
